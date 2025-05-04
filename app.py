65af606d3dcd31c8535558ffc8117394c2730b33
Anikyet <anikyetppatiyal@gmail.com>
2025-05-04 20:01:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 import os
 import tempfile
+from langchain.embeddings import HuggingFaceEmbeddings
 
 # Load environment variables
 load_dotenv()

b740a0c2d31018de1dc681e32de0d0e6e87fa94f
Aniket Patial <anikyetppatiyal@gmail.com>
2025-05-11 15:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ embeddings = HuggingFaceEmbeddings(
 # Load the image
 image = Image.open('image.png')
 
-# Resize the image (e.g., to 300x300 pixels)
-resized_image = image.resize((50, 50))
 
 
 # Streamlit UI
@@ -53,7 +51,7 @@ st.set_page_config(page_title="Intelleq", layout="wide")
 col1, col2 = st.columns([1, 3])
 # Display the resized image
 with col1:
-    st.image(resized_image, use_container_width=True)
+    st.image(resized_image, width=50)
 with col2:
     st.title("Intelleq")
 st.divider()

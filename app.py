113b24c24db32e54057859243cb373be37445491
Aniket Patial <anikyetppatiyal@gmail.com>
2025-11-25 12:31:23 +0530

    groq supported model update

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ st.subheader("How can I help you..?")
 st.sidebar.header("🔐 Configuration")
 selected_models = st.sidebar.multiselect(
     "Select one or more Open Source models",
-    ["gemma2-27b-it", "llama3.1-70b-versatile", "qwen2.5-72b-instruct", "llama3-70b-8192"],
-    default=["gemma2-27b-it"]
+    ["deepseek-r1-distill-llama-70b", "llama3.1-70b-versatile", "qwen2.5-72b-instruct", "llama3-70b-8192"],
+    default=["deepseek-r1-distill-llama-70b"]
 )
 
 if not selected_models:
@@ -339,6 +339,7 @@ if user_input:
     else:
         combined_reply = "\n\n".join(f"🔹 **{model}**: {reply}" for model, reply in responses.items())
         session_history.add_ai_message(combined_reply)
+
 
 
 
